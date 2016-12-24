ac508bf1de53c8f44899141a88154904ee280900
Alex <alex@sackclothstudios.com>
2016-12-24 00:28:00 -0800

    Initial commit.

---
+++ developer-discovery.js
@@ -0,0 +1,416 @@
+jQuery( document ).ready( function( $ ) {
+
+	// Creates the data display element.
+	$('body').prepend('<div id="dd-display"></div>');
+
+	// Creates the element to store modal content.
+	$('body').prepend('<div id="dd-modal" style="display: none;"></div>');
+
+	// Adds WordPress elements to the discovery system.
+	addWPelements();
+
+	// Adds links to expand descovery info.
+	$('.dd-definition').prepend('<a class="thmfdn-discovery-expand" href="#"></a>');
+
+	// Populates the data display element.
+	displayDataTree();
+
+	/**
+	 * Gets the tree of developer discoverable data
+	 *
+	 * @param element The HTML element from which to build the data tree.
+	 */
+	function calculateDataTree( element = $('#dd-data') ) {
+		data = [];
+		parentDefinition = '';
+
+		// Gets discoverable elements in current tree.
+		element.parents('.dd-definition').each(function( index ) {
+			// alert($(this).attr('data-discovery'));
+			definition = $(this).data('discovery');
+			// alert($(this).attr('data-discovery'));
+
+			// Is there any data for the definition table?
+			if (definition['table']) {
+
+				// Is the current template a parent?
+				if (definition['table']['Parent']) {
+					// Store the parent for insertion at a later time.
+					parentDefinition = definition;
+				} else {
+
+					// Is there a parent waiting to be inserted?
+					if (parentDefinition) {
+						definition['table']['Extends'] = parentDefinition['table']['Path'];
+						data.unshift(definition);
+						parentDefinition['table']['Extended By'] = definition['table']['Path'];
+						data.unshift(parentDefinition);
+						parentDefinition = '';
+					} else {
+						data.unshift(definition);
+					}
+				}
+			} else {
+				data.unshift(definition);
+			}
+		});
+
+		// Adds the base templates
+		baseTemplates = JSON.parse($('#dd-data').attr('data-discovery'));
+
+		extended = false;
+
+		if (baseTemplates.length > 1) {
+			extension = true;
+		} else {
+			extension = false;
+		}
+		$.each(baseTemplates, function( index, template ) {
+			className = '';
+
+			if ( extended ) {
+				// alert('Is extended: ' + template['handle']);
+				template['table']['Extended By'] = baseTemplates[index-1]['handle'];
+			}
+			if ( extension ) {
+				if (baseTemplates.length -2 == index) {
+					// alert(template['handle'] + ' creates cuttoff');
+					extension = false;
+				}
+				template['table']['Extends'] = baseTemplates[index+1]['handle'];
+				// alert('Is an extension: ' + template['handle']);
+				extended = true;
+			}
+
+			// display += '<a href="#" class="' + className + '" data-discovery-popup=\'' + JSON.stringify(template) + '\'>' + template['handle'] + '</a>';
+			// $.each(part, function( title, value ) {
+				// Just in case deeper details are needed here?
+			// });
+			data.unshift(template);
+		});
+
+		return data;
+	}
+
+	/**
+	 * Displays the developer discovery data
+	 *
+	 * @param data An array of discoverable elements.
+	 */
+	function displayDataTree( data = calculateDataTree() ) {
+		display = '';
+
+		// alert(JSON.stringify(data));
+
+		// Create link to reset data tree display
+		if (data.length > baseTemplates.length) {
+			display += '<a href="#" class="dd-reset">x</a>';
+		}
+
+		// Loops through discoverable elements in current tree.
+		$.each(data, function( index, part ) {
+			className = '';
+			// alert(index);
+			// alert(JSON.stringify(part));
+			if ( part['table'] ) {
+				if ( part['table']['Extended By'] ) {
+					className += ' extended';
+				}
+				if ( part['table']['Extends'] ) {
+					className += ' extension';
+				}
+			}
+			display += '<a href="#" class="' + className + '" data-discovery-popup=\'' + JSON.stringify(part) + '\'>' + part['handle'] + '</a>';
+			// $.each(part, function( title, value ) {
+				// Just in case deeper details are needed here?
+			// });
+		});
+
+		if (data.length > baseTemplates.length) {
+			$('#dd-display').addClass('expanded');
+		} else {
+			$('#dd-display').removeClass('expanded');
+		}
+
+		// Displays the new tree.
+		$('#dd-display').html(display);
+
+		// Resets the listener
+		modalListener();
+	}
+
+	/**
+	 * Sets the modal listener
+	 *
+	 * This code is placed within a function because it has to be called
+	 * each time the contents of the #dd-display element changes.
+	 */
+	function modalListener() {
+
+		// Generates and displays the discovery modal.
+		$('#dd-display > a').click(function(e) {
+			e.preventDefault();
+			$(this).css('outline', 'none');
+
+			if (!$(this).hasClass('dd-reset')) {
+				$('#dd-data').html(getModalContent($(this).attr('data-discovery-popup')));
+				$('#dd-data').modal();
+			}
+		});
+
+		// Resets the #dd-display element.
+		$('.dd-reset').click(function(e) {
+			e.preventDefault();
+			$('#dd-display').fadeOut('fast', function() {
+				displayDataTree();
+			});
+			$('#dd-display').fadeIn();
+		});
+	}
+
+
+	// Generates the discovery modal content.
+	function getModalContent(json) {
+
+		data = JSON.parse(json);
+
+		output = '';
+		// output += json;
+		output += '<h5>' + data['handle'] + '</h5>';
+		if (data['description']) {
+			output += '<p>' + data['description'] + '</p>';
+		}
+		output += '<table>';
+
+		$.each(data['table'], function( index, value ) {
+			// alert(index);
+			output += '<tr><th>'+index+'</th><td>'+value+'</td></tr>';
+			// $.each(part, function( title, value ) {
+				// Just in case deeper details are needed here?
+			// });
+		});
+		output += '</table>';
+
+		return output;
+
+	}
+
+	$('.dd-definition > a').click(function(e) {
+		e.preventDefault();
+		// $('.dd-selected').removeClass('dd-selected');
+		// $('.dd-current').addClass('dd-selected');
+		// alert('test');
+		$(this).css('outline', 'none');
+		// $('#dd-display').fadeOut('fast', function() {
+			displayDataTree(calculateDataTree($(this)));
+		// });
+		// $('#dd-display').fadeIn();
+	});
+
+	keys = {
+		d:      68,
+		h:      72,
+		esc:    27,
+	};
+
+	window.onkeyup = function(e) {
+
+		// Was a standard modifier key pressed?
+		if (e.altKey || e.ctrlKey) {
+			return true;
+		}
+
+		if ($('.dd-current').length) {
+			switch(e.keyCode) {
+					case keys.d: {
+						currentDataTree = calculateDataTree($('.dd-current > a'));
+						displayDataTree(currentDataTree);
+						break;
+					}
+					case keys.h: {
+						$('body').removeClass('dd-highlight');
+						break;
+					}
+					default: {
+						// alert('a different key was pressed');
+					}
+			}
+
+			e.stopPropagation();
+		} else {
+			return true;
+		}
+
+	}
+
+
+	window.onkeydown = function(e) {
+
+		// Was a standard modifier key pressed?
+		if (e.altKey || e.ctrlKey) {
+			return true;
+		}
+
+		if ($('.dd-current').length) {
+
+
+			switch(e.keyCode) {
+
+					case keys.h: {
+						$('body').addClass('dd-highlight');
+						break;
+					}
+
+					default: {
+						// alert('a different key was pressed');
+					}
+			}
+
+			e.stopPropagation();
+		} else {
+			return true;
+		}
+	}
+
+
+
+	function addWPelements() {
+
+		details = {};
+		details.handle ='the_post_navigation()';
+		details.table = {
+			'Type': 'WordPress function',
+			'Docs': '<a href="https://developer.wordpress.org/reference/functions/the_post_navigation/">https://developer.wordpress.org/reference/functions/the_post_navigation/</a>'
+		};
+		detailsJSON = JSON.stringify(details);
+		// alert(doo);
+		$('.post-navigation').wrap('<div class="dd-definition" data-discovery=\'' + detailsJSON + '\'></div>');
+		// $('.post-navigation').each(function() {
+		// 	// test = $(this).data('discovery');
+		// 	// alert(test['handle']);
+		// });
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		$('.dd-definition').on('mouseover', function (event) {
+
+			// Prevent event from bubbling up to parent elements.
+			event.stopPropagation();
+
+			// Remove any previously "current" particles.
+			$('.dd-current').removeClass('dd-current');
+
+			// Mark the current particle.
+			$(this).addClass('dd-current');
+
+
+			// detailsJSON = $(this).find('.thmfdn-discovery-expand').attr('data-details');
+			// dataDetails = JSON.parse(detailsJSON);
+			// $('#thmfdn-discovery-box').html('<a href="#" class="thmfdn-footnoter" data-details=\'' + detailsJSON + '\'>' + dataDetails['path'] + '</a>');
+
+
+
+			// console.log('Capture for ' + event.type + ' target is ' + overID);
+		});
+
+	$(function () {
+		$('.thmfdn-discovery').on('mouseout', function (event) {
+			event.stopPropagation();
+
+			// $('.thmfdn-discovery-expand').hide();
+			$(this).removeClass('dd-current');
+			// $(this).children('.thmfdn-discovery-expand').hide( 200 );
+
+
+			outID = $(this).children('.thmfdn-discovery-define').attr('id');
+			// if (overID == outID) {
+			// 	// alert(overID);
+			// } else {
+
+			// }
+			// console.log('Capture for ' + event.type + ' target is ' + outID);
+		});
+	});
+
+
+	function thmfdnPopulateModal(json) {
+		data = JSON.parse(json);
+
+		output = '';
+
+		output += '<div id="thmfdn-modal-details" style="display: none;">';
+		output += '<h5>' + data['title'] + '</h5>';
+		output += '</div>';
+
+
+		return output;
+
+	}
+
+
+
+
+
+	// var expandingLocations  = new Object();
+
+
+	// $('.thmfdn-discovery').hover(function() {
+	// 	currentID = $(this).children('.thmfdn-discovery-define').attr('id');
+	// 	currentTop = $(this).offset().top;
+	// 	// alert(currentTop);
+	// 	expandingLocations[currentID] = currentTop;
+
+	// 	// alert($(this).find('.thmfdn-discovery-define').attr('id'));
+	// 	// alert(currentID);
+
+	// 	// alert(thisLocation);
+	// 	// alert(expandingLocations);
+	// 	$.each(expandingLocations, function( index, value ) {
+	// 		if ( index != currentID ) {
+	// 			if ( value == currentTop ) {
+	// 				// $(this).find('.thmfdn-discovery-expand').css('z-index', '10000000');
+	// 				// alert( index + ": " + value + " matches " + currentID + ": " + currentTop );
+	// 				// alert($('#'+index).siblings('.thmfdn-discovery-expand').attr('href'));
+	// 				newZ = $('#'+currentID).siblings('.thmfdn-discovery-expand').css('z-index') - 10;
+	// 				$('#'+currentID).siblings('.thmfdn-discovery-expand').css({'width': '64px', 'height': '64px', 'border-bottom-right-radius': '80px', 'z-index': newZ});
+	// 				// $('#'+index).find('.thmfdn-discovery-expand').hide(1000);
+	// 			} else {
+	// 				// alert( index + " no match " + value );
+	// 			}
+	// 		}
+	// 	});
+	// 	$(this).children('.thmfdn-discovery-expand').show( 200 );
+
+	// 	// alert(JSON.stringify(expandingLocations));
+	// 	// test[test.length] = $(this).find('.thmfdn-discovery-expand').offset().left;
+	// 	// alert( $(this).find('.thmfdn-discovery-expand').offset().top );
+
+	// },function(){
+	// 	$(this).find('.thmfdn-discovery-expand').hide( 200 );
+	// 	formerID = $(this).children('.thmfdn-discovery-define').attr('id');
+	// 	delete expandingLocations[formerID];
+	// 	// alert(JSON.stringify(expandingLocations));
+
+	// });
+
+});
---
+++ developer-discovery.php
@@ -0,0 +1,438 @@
+<?php
+/*
+Plugin Name: Developer Discovery
+Plugin URI: http://themefoundation.com
+Description: This plugin displays additional developer information on the front end.
+Author: Alex Mansfield
+Version: 0.1.0
+Author URI: http://themefoundation.com
+
+@see http://jquerymodal.com/
+*/
+class THMFDN_Developer_Discovery {
+
+	/**
+	 * Class constructor
+	 *
+	 * Adds actions/filters to hooks.
+	 */
+	function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+		add_action( 'wp_footer', array( $this, 'developer_discovery_div') );
+
+		add_action( 'thmfdn_particle_before', array( $this, 'thmfdn_discovery_particle_before') );
+		add_action( 'thmfdn_particle_after', array( $this, 'thmfdn_discovery_after') );
+		add_action( 'thmfdn_template_part_before',  array( $this, 'thmfdn_discovery_template_part_before'), 10, 3 );
+		add_action( 'thmfdn_template_part_after',  array( $this, 'thmfdn_discovery_after') );
+		add_action( 'thmfdn_extend_template_before',  array( $this, 'thmfdn_discovery_extend_template_before'), 10, 1 );
+
+		add_action( 'thmfdn_comments_before',  array( $this, 'thmfdn_discovery_comments_before') );
+		add_action( 'thmfdn_comments_after',  array( $this, 'thmfdn_discovery_after') );
+
+		add_action( 'dynamic_sidebar_before', array( $this, 'sidebar_before') );
+		add_action( 'dynamic_sidebar_after', array( $this, 'thmfdn_discovery_after') );
+	}
+
+	/**
+	 * Enqueues styles and scripts
+	 */
+	public function enqueue() {
+		wp_enqueue_style( 'jquery-modal',  plugin_dir_url( __FILE__ ) . 'jquery.modal.css' );
+		wp_enqueue_style( 'developer-discovery',  plugin_dir_url( __FILE__ ) . 'developer-discovery.css' );
+
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'jquery-modal',  plugin_dir_url( __FILE__ ) . 'jquery.modal.js', array( 'jquery' ), false, true );
+		wp_enqueue_script( 'developer-discovery',  plugin_dir_url( __FILE__ ) . 'developer-discovery.js', array( 'jquery' ), false, true );
+	}
+
+	/**
+	 * Creates developer discover div
+	 *
+	 * The plugin populates this div with content as needed.
+	 */
+	public function developer_discovery_div() {
+		global $thmfdn_developer_discovery;
+		if ( empty( $thmfdn_developer_discovery ) ) {
+			$thmfdn_developer_discovery = array();
+		}
+
+		$details['path'] = $this->get_current_template();
+		$details['handle'] = basename( $details['path'] );
+		$split_path = explode( 'wp-content/', $details['path'] );
+		$details['table']['Path'] = end( $split_path );
+
+		array_unshift( $thmfdn_developer_discovery, $details );
+		$json_details =  json_encode( $thmfdn_developer_discovery );
+		//echo wpautop( thmfdn_get_file_doc_block(thmfdn_get_current_template()));
+		?>
+			<div id="dd-data" data-discovery='<?php echo $json_details; ?>'></div>
+		<?php
+	}
+
+	/**
+	 * Gets the path to the current template file, as determined by WordPress
+	 *
+	 * @see http://wordpress.stackexchange.com/questions/10537/get-name-of-the-current-template-file
+	 */
+	public function get_current_template() {
+		if( !empty( $GLOBALS['current_theme_template'] ) ) {
+			$template = $GLOBALS['current_theme_template'];
+		} else {
+			$template = false;
+		}
+
+		return $template;
+	}
+
+	/**
+	 * Opens particle wrapper div
+	 */
+	public function thmfdn_discovery_particle_before( $id ) {
+		$details = $this->get_particle_details( $id );
+		$details_json = json_encode( $details );
+		$class = 'thmfdn-discovery dd-definition';
+		if ( !empty( $details['display'] ) && 'inline' == $details['display'] ) {
+			$class .= ' dd-inline';
+		}
+		echo '<div class="' . $class . '" data-discovery=\'' . $details_json . '\'>';
+		// echo '<a class="thmfdn-discovery-expand" href="#"></a>';
+	}
+
+	/**
+	 * Opens template part wrapper div
+	 */
+	public function thmfdn_discovery_template_part_before( $slug, $name = '', $parent = false ) {
+		echo '<div class="thmfdn-discovery dd-definition" data-discovery=\'' . $this->get_template_part_details_json( $slug, $name, $parent ) . '\'>';
+		// echo '<a class="thmfdn-discovery-expand" href="#"></a>';
+	}
+
+	/**
+	 * Opens sidebar wrapper div
+	 */
+	public function sidebar_before( $id ) {
+		global $wp_registered_sidebars;
+
+		// Gets sidebar details.
+		$details = array();
+		$details['title'] = $wp_registered_sidebars[$id]['name'];
+		// $details['description'] = 'This function calls a registered sidebar.';
+		// $details['path'] = locate_template( $template_name, false );
+		$details['table']['Name'] = $wp_registered_sidebars[$id]['name'];
+		$details['table']['Description'] = $wp_registered_sidebars[$id]['description'];
+ 		$details['table']['Type'] = 'WordPress function';
+ 		$details['table']['Docs'] = '<a href="https://developer.wordpress.org/reference/functions/dynamic_sidebar/">https://developer.wordpress.org/reference/functions/dynamic_sidebar/</a>';
+		// $path_array = explode( 'wp-content/', $details['path'] );
+ 		// $details['table']['Path'] = end( $path_array );
+		$details['handle'] = 'dynamic_sidebar()';
+		// echo '<pre>';
+		// print_r($wp_registered_sidebars[$id]);
+		// echo '</pre>';
+		// $details = $this->get_particle_details( $id );
+		$details_json = json_encode( $details );
+		$class = 'thmfdn-discovery dd-definition';
+		// if ( !empty( $details['display'] ) && 'inline' == $details['display'] ) {
+		// 	$class .= ' dd-inline';
+		// }
+		echo '<div class="' . $class . '" data-discovery=\'' . $details_json . '\'>';
+		// echo '<a class="thmfdn-discovery-expand" href="#"></a>';
+	}
+
+	public function thmfdn_discovery_extend_template_before( $template_name ) {
+		global $thmfdn_developer_discovery;
+
+		// Gets template details.
+		$details = array();
+		$details['title'] = 'Template File';
+		$details['path'] = locate_template( $template_name, false );
+ 		$details['table']['Type'] = 'Template';
+		$path_array = explode( 'wp-content/', $details['path'] );
+ 		$details['table']['Path'] = end( $path_array );
+		$details['handle'] = basename( $template_name );
+
+		$thmfdn_developer_discovery[] = $details;
+	}
+
+	/**
+	 * Opens comments wrapper div
+	 */
+	public function thmfdn_discovery_comments_before() {
+		$discovery_details = array(
+			'path' => 'comments.php',
+			'handle' => 'comments.php',
+			'description' => 'This is the comments template. WordPress loads this template using the comments_template() function.',
+			'table' => array(
+				'Docs' => '<a href="https://developer.wordpress.org/reference/functions/comments_template/">https://developer.wordpress.org/reference/functions/comments_template/</a>',
+			),
+		);
+		echo '<div class="thmfdn-discovery dd-definition" data-discovery=\'' . json_encode( $discovery_details ) . '\'>';
+		// echo '<a class="thmfdn-discovery-expand" href="#"></a>';
+	}
+
+	/**
+	 * Closes wrapper div
+	 */
+	public function thmfdn_discovery_after() {
+		echo '</div>';
+	}
+
+	/**
+	 * Gets details about a particle
+	 */
+	public function get_particle_details( $id ) {
+		global $thmfdn_particles;
+
+		// Gets path and file name.
+		$reflection = new ReflectionFunction($thmfdn_particles->particles[$id]['function']);
+		$details['path'] = $reflection->getFileName();
+		$details['handle'] = basename( $details['path'] );
+		if ( !empty( $thmfdn_particles->particles[$id]['display'] ) ) {
+			$details['display'] = $thmfdn_particles->particles[$id]['display'];
+		}
+
+
+		// Sets up args capture.
+		$arg_string = '';
+		$arg_first = true;
+
+		// Loops through args.
+		if ( !empty(  $thmfdn_particles->particles[$id]['args'] ) && is_array(  $thmfdn_particles->particles[$id]['args'] ) ) {
+			foreach ( $thmfdn_particles->particles[$id]['args'] as $arg ) {
+				if ( $arg_first ) {
+					$arg_string .= '$' . $arg;
+					$arg_first = false;
+				} else {
+					$arg_string .= ', $' . $arg;
+				}
+			}
+		}
+
+		// Gets additional particle details
+		$details['table']['Type'] = 'Particle';
+		$details['table']['Title'] = $thmfdn_particles->particles[$id]['title'];
+		$details['table']['Description'] = $thmfdn_particles->particles[$id]['description'];
+		$path_array = explode( 'wp-content/', $details['path'] );
+ 		$details['table']['Path'] = end( $path_array );
+		$details['table']['Function'] = $thmfdn_particles->particles[$id]['function'] . '( ' . $arg_string . ' )';
+		// $details['table']['Line #'] = $reflection->getStartLine();
+
+		return $details;
+	}
+
+	/**
+	 * Gets details about a template part
+	 */
+	public function get_template_part_details_json( $slug, $name = '', $parent = false ) {
+
+		// Gets template part details.
+		$details = array();
+		$details['title'] = 'Template Part';
+		$details['table']['Type'] = 'Template Part';
+		$details['table']['Path'] = ( empty( $name ) ) ? $slug . '.php' : $slug . '-' . $name . '.php';
+		$details['table']['Slug'] = $slug;
+		$details['table']['Name'] = $name;
+		$details['table']['Parent'] = $parent;
+		$details['handle'] = basename( $details['table']['Path'] );
+
+		return json_encode( $details );
+	}
+
+
+}
+
+/**
+ * Initializes the plugin class if conditions are met
+ */
+function thmfdn_developer_discovery_init() {
+	if ( !is_admin() ) {
+		if ( current_user_can( 'manage_options' ) ) {
+			if ( 'on' == get_user_option( 'developer_discovery_status' ) ) {
+				$thmfdn_developer_discovery = new THMFDN_Developer_Discovery;
+			}
+		}
+	}
+}
+add_action( 'init', 'thmfdn_developer_discovery_init' );
+
+/**
+ * Stores the path to the current template file globally for later use
+ */
+function thmfdn_store_current_template( $template ){
+	$GLOBALS['current_theme_template'] = $template;
+	return $template;
+}
+add_filter( 'template_include', 'thmfdn_store_current_template', 1000 );
+
+/**
+ * Adds discovery toggle to WP admin bar
+ */
+function thmfdn_developer_discovery_toggle( $wp_admin_bar ) {
+	if ( !is_admin() ) {
+
+		$current_discovery_status = get_user_option( 'developer_discovery_status' );
+
+		if ( empty( $current_discovery_status ) || 'off' == $current_discovery_status ) {
+			$toggled_discovery_status = 'on';
+			$button_title = 'Turn Discovery On';
+		} else {
+			$toggled_discovery_status = 'off';
+			$button_title = 'Turn Discovery Off';
+		}
+		$args = array(
+			'id' => 'dd-toggle',
+			'title' => $button_title,
+			'href' => get_permalink() . '?developer-discovery-status=' . $toggled_discovery_status,
+			'parent' => 'top-secondary',
+			'meta' => array(
+				'class' => 'dd-display-toggle'
+			)
+		);
+		$wp_admin_bar->add_node($args);
+	}
+}
+add_action( 'admin_bar_menu', 'thmfdn_developer_discovery_toggle', 50 );
+
+
+/**
+ * Updates saved discovery toggle state when changed by user
+ */
+function thmfdn_developer_discovery_update_toggle_state() {
+	if ( !empty( $_GET['developer-discovery-status'] ) ) {
+
+		if ( 'on' == $_GET['developer-discovery-status'] ) {
+			update_user_option( get_current_user_id(), 'developer_discovery_status', 'on' );
+		}
+
+		if ( 'off' == $_GET['developer-discovery-status'] ) {
+			update_user_option( get_current_user_id(), 'developer_discovery_status', 'off' );
+		}
+	}
+}
+add_action( 'init',  'thmfdn_developer_discovery_update_toggle_state', 1);
+
+
+
+
+
+
+
+
+
+
+
+// $thmfdn_developer_discovery_count = 0;
+
+
+
+// function thmfdn_discovery_template_part_bottom() {
+// 	if ( current_user_can( 'manage_options' ) ) {
+// 		echo '</div>';
+// 	}
+// }
+// add_action( 'thmfdn_template_part_bottom', 'thmfdn_discovery_template_part_bottom' );
+
+
+// function thmfdn_get_get_particle_details_json( $id ) {
+// 		global $thmfdn_particles;
+// 		$details = array();
+
+// 		// Get file path;
+// 		$reflection = new ReflectionFunction($thmfdn_particles->particles[$id]['function']);
+// 		$full_path = $reflection->getFileName();
+// 		$split_path = explode( 'wp-content/', $full_path );
+// 		$useful_path = end( $split_path );
+
+// 		$arg_string = '';
+// 		$arg_first = true;
+
+// 		foreach ( $thmfdn_particles->particles[$id]['args'] as $arg ) {
+// 			if ( $arg_first ) {
+// 				$arg_string .= '$' . $arg;
+// 				$arg_first = false;
+// 			} else {
+// 				$arg_string .= ', $' . $arg;
+// 			}
+// 		}
+// // echo '&&' . $thmfdn_particles->particles[$id]['title'] . '&&';
+// 		$details['title'] = $thmfdn_particles->particles[$id]['title'];
+// 		$details['description'] = $thmfdn_particles->particles[$id]['description'];
+// 		$details['path'] = $useful_path;
+
+// 		$json_details =  json_encode( $details );
+// // echo '&&' . $json_details . '&&';
+
+// 		return $json_details;
+// }
+
+
+
+// function thmfdn_get_discovery_particle_details( $id ) {
+// 		global $thmfdn_particles;
+
+// 		// print_r(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
+// 		// $e = new \Exception;
+// 		// var_dump($e->getTraceAsString());
+// 		//http://php.net/manual/en/exception.gettrace.php
+
+// 		// $bt = debug_backtrace();
+// 		// print_r($bt);
+// 		// $caller = array_shift($bt);
+// 		// echo $bt[0]['file'];
+// 		// wp-content/
+
+// 		$reflection = new ReflectionFunction($thmfdn_particles->particles[$id]['function']);
+// 		$full_path = $reflection->getFileName();
+// 		$split_path = explode( 'wp-content/', $full_path );
+// 		$useful_path = end( $split_path );
+
+// 		$arg_string = '';
+// 		$arg_first = true;
+
+// 		foreach ( $thmfdn_particles->particles[$id]['args'] as $arg ) {
+// 			if ( $arg_first ) {
+// 				$arg_string .= '$' . $arg;
+// 				$arg_first = false;
+// 			} else {
+// 				$arg_string .= ', $' . $arg;
+// 			}
+// 		}
+
+
+// 		// echo '<pre>';
+// 		// print_r($thmfdn_particles->particles[$id]);
+// 		// print_r(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
+// 		// $e = new \Exception;
+// 		// var_dump($e->getTraceAsString());
+// 		//http://php.net/manual/en/exception.gettrace.php
+
+// 		// $bt = debug_backtrace();
+// 		// print_r($bt);
+// 		// $caller = array_shift($bt);
+// 		// echo $bt[0]['file'];
+// 		// wp-content/
+
+
+
+// 		// $reflection = new ReflectionFunction($thmfdn_particles->particles[$id]['function']);
+// 		// echo $reflection->getFileName() . ':' . $reflection->getStartLine();
+// 		// echo '</pre>';
+// }
+
+
+
+
+
+/**
+ * Gets the first docblock in a file
+ *
+ * @see http://stackoverflow.com/questions/11504541/get-comments-in-a-php-file
+ */
+// function thmfdn_get_file_doc_block( $file_path )
+// {
+// 	$docComments = array_filter(
+// 		token_get_all( file_get_contents( $file_path ) ), function($entry) {
+// 			return $entry[0] == T_DOC_COMMENT;
+// 		}
+// 	);
+// 	$fileDocComment = array_shift( $docComments );
+// 	return $fileDocComment[1];
+// }
